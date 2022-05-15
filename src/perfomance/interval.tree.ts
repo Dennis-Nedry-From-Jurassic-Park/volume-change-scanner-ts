49853dcddd3ae8420db2d23c383bb58b349d4bf3
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2022-05-15 15:04:13 +0300

    Interval tree # 1 (#18)
    * interval tree libs:
    ibst = call stack exceed after 1 mi elements
    interval-tree-1d = var r = cb(arr[i]); cb is not a function
    interval-tree-type = 1bi elems per second search (win)
    roa-pool = wrr-round-robin-pool ~ 40-50 thousand elems per sec

---
+++ src/perfomance/interval.tree.ts
@@ -0,0 +1,80 @@
+import {roaPool} from '../pools/efficiency.pool';
+
+import {v4 as uuidv4} from 'uuid';
+
+let uuid = uuidv4();
+
+const b = require('benny')
+
+var Integer = require('integer');
+var IntervalTree = require("interval-tree-type");
+
+
+const createIntervalTree = (range: typeof Integer): typeof IntervalTree => {
+    const tree = new IntervalTree();
+    let i = Integer(range + 1);
+    const PRECISION = 1e-8;
+    let step = 0.005;
+
+    while ( --i ){
+        tree.insert(i-1, i, step);
+        step++;
+    }
+
+    return tree;
+}
+
+const numberElements = 1_000_000;
+const searchElement = numberElements / 2;
+
+var IntervalTree2 = require('interval-tree2');
+
+
+const tree = createIntervalTree(Integer(numberElements));
+
+const create_intervals = (range: number): typeof IntervalTree2 => {
+    let temp_tree = new IntervalTree2(searchElement);
+    let i = range+1;
+    const PRECISION = 1e-8;
+    let step = 0.005;
+
+    while ( --i ){
+         temp_tree.add(i-1, i-PRECISION, step);
+         step++;
+    }
+
+    return temp_tree;
+}
+
+var itree = create_intervals(numberElements)
+
+const options = {
+  minSamples: 5,
+  delay: 0.0025
+}
+
+const suiteName = 'interval-tree'
+
+b.suite(
+  suiteName,
+
+  b.add('IntervalTree', () => {
+          tree.queryPoint(searchElement);
+    }, options),
+
+  b.add('itree', () => {
+        itree.pointSearch(searchElement);
+  b.add('roaPool', () => {
+        roaPool.getROAWeight(999.99);
+    }, options),
+
+  b.add('tree_wo_weights', () => {
+        tree_wo_weights.queryPoint(searchElement);
+    }, options),
+
+  b.cycle(),
+  b.complete(),
+
+  b.save({ file: suiteName + uuid, version: '1.0.0' }),
+  b.save({ file: suiteName + uuid, format: 'chart.html' }),
+)
