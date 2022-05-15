eebd2ac12bc6ae752d326ab0a869ec223316371d
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2022-05-15 13:25:32 +0300

    check roaPool

--- src/debug.ts
+++ src/debug.ts
@@ -1,17 +1,14 @@
 import {altmanZscorePool} from './pools/ratings.pool';
-import {ROApool} from './pools/efficiency.pool';
+import {roaPool} from './pools/efficiency.pool';
 import clickhouse from './clickhouse/clickhouse';
 import performSql from './clickhouse/perform-sql';
-//import {memorySizeOf} from './debug/memorySizeOfObject';
 import sizeof from 'object-sizeof';
-//const ROApool = require('./pools/efficiency/ROA').ROApool;
-
 
 const fn = async () => {
 	const a = [1,2,3];
 
-	console.log(ROApool.getWeightedBuffetIndicator());
-	//console.log(ROApool.getROAweight(999.99));
+	console.log(roaPool.getWeightedBuffetIndicator());
+	console.log(roaPool.getROAWeight(999.99));
 	console.log(altmanZscorePool.getWeightedBuffetIndicator());
 	console.log(sizeof(a));
 };
