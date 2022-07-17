6960366fa72d01dd5a4c5db3ee0af495dfef8b2c
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2022-07-17 12:53:23 +0300

    update: .gitignore - /benchmark/
    add:
    1. round-robin-pool
    -  111.ts rbush lib;
    -  111.ts SegmentTree lib structures-wiz;
    -  cycle.ts AVLTree lib @foxglove/avl
    =  rbush+AVLTree+SegmentTree stack size exceed
    2. wrr-pool algorithm (very slow compared to with any implementation of tree structs - git save for history)
    -  gcd.perf.ts
    -  gcd0.perf.ts
    -  max.by.perf.ts
    -  max.by.ts
    -  pool.test.ts
    -  pool.ts
    3. ROA pool for history
    4. WRR pool for history
    wrr.*.pool.ts

--- src/bounds/score.ts
+++ src/bounds/score.ts
@@ -2,7 +2,7 @@ import {InvestmentIndicatorBoundScore} from './bound.record';
 
 export const scoreBoundRange :
     Record<string, InvestmentIndicatorBoundScore> = {
-    	AltmanZscore: new InvestmentIndicatorBoundScore ('AltmanZscore', 3.5,   true, '>='),
-    	PiotroskiFscore: new InvestmentIndicatorBoundScore ('PiotroskiFscore', 7.0,   true, '>='),
-    	BeneishMscore: new InvestmentIndicatorBoundScore ('BeneishMscore', -3.0,   false, '<='),
+    	AltmanZscore: new InvestmentIndicatorBoundScore ('Altman Z-Score', 3.5,   true, '>='),
+    	PiotroskiFscore: new InvestmentIndicatorBoundScore ('Piotroski F-Score', 7.0,   true, '>='),
+    	BeneishMscore: new InvestmentIndicatorBoundScore ('Beneish M-Score', -3.0,   false, '<='),
     };
--- src/debug.ts
+++ src/debug.ts
@@ -1,18 +1,20 @@
 import {altmanZscorePool} from './pools/ratings.pool';
-import {ROApool} from './pools/efficiency.pool';
+import {roaPool} from './pools/efficiency.pool';
 import clickhouse from './clickhouse/clickhouse';
 import performSql from './clickhouse/perform-sql';
 //import {memorySizeOf} from './debug/memorySizeOfObject';
 import sizeof from 'object-sizeof';
-//const ROApool = require('./pools/efficiency/ROA').ROApool;
+//const roaPool = require('./pools/efficiency/ROA').roaPool;
 
 
 const fn = async () => {
 	const a = [1,2,3];
 
-	console.log(ROApool.getWeightedBuffetIndicator());
-	//console.log(ROApool.getROAweight(999.99));
-	console.log(altmanZscorePool.getWeightedBuffetIndicator());
+
+	//console.log(roaPool.getROAweight(999.99));
+	//console.log(altmanZscorePool.getWeightedBuffetIndicator());
+	console.log(roaPool.getROAWeight0(999.99));
+	console.log(roaPool.getROAWeight(999.99));
 	console.log(sizeof(a));
 };
 
