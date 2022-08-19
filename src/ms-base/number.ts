8484457426876dee35a321d4d8ec075215a7a09c
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2022-08-19 12:43:57 +0300

    rm ms-base moved to ms-ti-base

--- src/ms-base/number.ts
+++
@@ -1,9 +0,0 @@
-import {MoneyValue, Quotation} from "tinkoff-invest-api/cjs/generated/common";
-import {api} from "../api.ti.common";
-
-export const toNum = (value:Quotation | MoneyValue | undefined): number | undefined => {
-    return api.helpers.toNumber(value) ;
-}
-export const toQuotation = (value: number): Quotation => {
-    return api.helpers.toQuotation(value) ;
-}
--- src/ms-base/output.ts
+++
@@ -1,3 +0,0 @@
-export const prettyJSON = (str:any): string => {
-    return JSON.stringify(str, null, 4);
-}
