d348146adf0c9f9a63ac335313ebdfb979b8d9c9
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2022-08-20 13:20:37 +0300

    price change algorithm check

--- src/ms-change-price-strategy/russian.shares.all.ts
+++ src/ms-change-price-strategy/russian.shares.all.ts
@@ -8,15 +8,14 @@ import {instrumentsService} from "../ms-ti-base/instruments.service";
 
 export const price_change_rus_shares = async () => {
     const rus_shares = await instrumentsService.get_all_russian_shares();
-    const tickers = rus_shares.map((it:Share) => { return it.ticker } )
-        //const tickers = instrumentsService.get_tickers()
+    const tickers = rus_shares.map((it:Share) => { return it.ticker } );
     await get_price_change(Exchange.MOEX, tickers, CandleInterval.CANDLE_INTERVAL_DAY);
 }
 
-price_change_rus_shares()
-    .then(() => console.log('get price changes for russian shares with apiTradeAvailableFlag (=false|true)'));
+//price_change_rus_shares()
+//    .then(() => console.log('get price changes for russian shares with apiTradeAvailableFlag (=false|true)'));
 
-delay(2500).then(() => console.log('delay between runs get price changes for russian shares'))
+//delay(2500).then(() => console.log('delay between runs get price changes for russian shares'))
 
 //price_change_rus_shares(true)
 //    .then(() => console.log('get price changes for russian shares with apiTradeAvailableFlag (=true)'));
