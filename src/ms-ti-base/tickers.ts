d5e823eab011ee4c02af61d42e3dacae7931915a
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2022-08-23 12:34:55 +0300

    export tickers tinkoff investments

---
+++ src/ms-ti-base/tickers.ts
@@ -0,0 +1,5 @@
+const tickers: string[] =  require("../ms-crawler/spbe.10_00_main_session.tickers.json");
+//import * as tickers from "../ms-crawler/spbe.10_00_main_session.tickers.json";
+export {
+    tickers
+}
