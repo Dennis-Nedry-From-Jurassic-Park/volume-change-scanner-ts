c3b1a07196fd3fd0711ce666530d56fa84c035c2
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2022-08-23 12:33:18 +0300

    export const default_retries = 3; export const default_retry_delay = 5000; default delay constants

--- src/ms-ti-base/wait.ts
+++ src/ms-ti-base/wait.ts
@@ -6,3 +6,6 @@ export const delay = (ms:number) => new Promise(resolve => setTimeout(resolve, m
 export async function waitMarketStreamEvent(api: TinkoffInvestApi, event: 'data' | 'close') {
     return new Promise<MarketDataResponse>(resolve => api.stream.market.on(event, resolve));
 }
+
+export const default_retries = 3;
+export const default_retry_delay = 5000;
