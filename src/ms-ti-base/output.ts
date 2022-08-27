2744348f692690ddeb2bb7246c7dbd6c67e3a98d
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2022-08-27 16:38:55 +0300

    prettyJSONbigint

--- src/ms-ti-base/output.ts
+++ src/ms-ti-base/output.ts
@@ -1,3 +1,7 @@
+export const prettyJSONbigint = (str:any): string => {
+    return JSON.stringify(str, (_, v) => typeof v === 'bigint' ? v.toString() : v)
+}
 export const prettyJSON = (str:any): string => {
     return JSON.stringify(str, null, 4);
 }
+
