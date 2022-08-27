fb672e709c8070ce930ab6a6779dbbaea3c2bc77
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2022-08-27 16:33:46 +0300

    cron

--- src/ms-cron/bull.ts
+++ src/ms-cron/bull.ts
@@ -7,10 +7,12 @@ import {
 } from "../ms-change-price-strategy/prepare-candles-sbpe-exchange-main-session";
 import {logger_cron, logger_market_depth} from "../logger/logger";
 import WaitJob from "./wait-job";
+import moment from "moment";
 
 const Queue = require('bull');
 
-const waitJob = new WaitJob('08:22:30')
+// run clickhouse and redis
+const waitJob = new WaitJob('08:57:30')
 
 const firstJobAt = waitJob.getFirstJobAt();
 const secondJobAt = waitJob.getSecondJobAt();
@@ -28,46 +30,46 @@ const data = {};
 
 const options_for_moex_exchange_queue = {
     delay: 5000,
-    attempts: 3,
+    attempts: 2,
     repeat: { cron: cron_moex_exchange }
 };
 
 prepare_candles_moex_exchange_queue.add(data, options_for_moex_exchange_queue);
 prepare_candles_moex_exchange_queue.process((job) => prepare_candles_moex_exchange());
 prepare_candles_moex_exchange_queue.on('completed', job => {
-    logger_cron.log('debug', `Job prepare_candles_moex_exchange_queue with id ${job.id} has been completed`);
+    logger_cron.log('debug', moment().format('HH:mm:ss') + `Job prepare_candles_moex_exchange_queue with id ${job.id} has been completed`);
 })
 prepare_candles_moex_exchange_queue.on('failed', job => {
-    logger_cron.error(`Job prepare_candles_moex_exchange_queue with id ${job.id} has been failed`);
+    logger_cron.error(moment().format('HH:mm:ss') + ` Job prepare_candles_moex_exchange_queue with id ${job.id} has been failed`);
 })
 
 const options_for_spbe_exchange_morning_session_queue = {
     delay: 5000,
-    attempts: 3,
+    attempts: 2,
     repeat: { cron: cron_spbe_exchange_morning_session }
 };
 
 prepare_candles_spbe_exchange_morning_session_queue.add(data, options_for_spbe_exchange_morning_session_queue);
 prepare_candles_spbe_exchange_morning_session_queue.process((job) => prepare_candles(tickers));
 prepare_candles_spbe_exchange_morning_session_queue.on('completed', job => {
-    logger_cron.log('debug', `Job prepare_candles_spbe_exchange_morning_session_queue with id ${job.id} has been completed`);
+    logger_cron.log('debug', moment().format('HH:mm:ss') + `Job prepare_candles_spbe_exchange_morning_session_queue with id ${job.id} has been completed`);
 })
 prepare_candles_spbe_exchange_morning_session_queue.on('failed', job => {
-    logger_cron.error(`Job prepare_candles_spbe_exchange_morning_session_queue with id ${job.id} has been failed`);
+    logger_cron.error(moment().format('HH:mm:ss') + `Job prepare_candles_spbe_exchange_morning_session_queue with id ${job.id} has been failed`);
 })
 
 const options_for_spbe_exchange_main_session_queue = {
     delay: 5000,
-    attempts: 3,
+    attempts: 2,
     repeat: { cron: cron_spbe_exchange_main_session }
 };
 
 prepare_candles_spbe_exchange_main_session_queue.add(data, options_for_spbe_exchange_main_session_queue);
 prepare_candles_spbe_exchange_main_session_queue.process((job) => insert_candles_to_all_usa_shares_except_morning_session());
 prepare_candles_spbe_exchange_main_session_queue.on('completed', job => {
-    logger_cron.log('debug', `Job prepare_candles_spbe_exchange_main_session_queue with id ${job.id} has been completed`);
+    logger_cron.log('debug', moment().format('HH:mm:ss') + `Job prepare_candles_spbe_exchange_main_session_queue with id ${job.id} has been completed`);
 })
 prepare_candles_spbe_exchange_main_session_queue.on('failed', job => {
-    logger_cron.error(`Job prepare_candles_spbe_exchange_main_session_queue with id ${job.id} has been failed`);
+    logger_cron.error(moment().format('HH:mm:ss') + `Job prepare_candles_spbe_exchange_main_session_queue with id ${job.id} has been failed`);
 })
 
