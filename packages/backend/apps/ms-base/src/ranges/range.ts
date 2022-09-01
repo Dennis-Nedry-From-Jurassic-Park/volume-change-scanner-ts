224dea5c7e14cf71f79026abe2f6d5a09309ecd2
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2022-09-01 20:16:02 +0300

    npm to yarn ratings.pool

---
+++ packages/backend/apps/ms-base/src/ranges/range.ts
@@ -0,0 +1,23 @@
+export function rangeInclusive (start:number, stop:number, stepSize:number) {
+	if (stop == null) {
+		stop = start;
+		start = 1;
+	}
+	if (stepSize == null) stepSize = 1;
+
+	const steps = (stop - start) / stepSize;
+
+	const set: number[] = [];
+	for (let step = 0; step <= steps; step++) set.push(start + step * stepSize);
+
+	return set;
+}
+
+export function rangeExclusive (start:number, stop:number, stepSize:number) : any {
+	//if (stop == null) return rangeExclusive(1, start)
+	if (stepSize == null) return rangeExclusive(start, stop, 1);
+
+	const range = rangeInclusive(start, stop, stepSize);
+	if (stop % stepSize === 0) range.pop();
+	return range;
+}
