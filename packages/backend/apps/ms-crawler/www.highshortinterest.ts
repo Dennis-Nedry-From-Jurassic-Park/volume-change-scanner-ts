29e8df8cd01dcd2a4a67884a909ba16f50e3bbb4
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2022-09-05 17:15:08 +0300

    moved to another repos atr-*

--- packages/backend/apps/ms-crawler/www.highshortinterest.ts
+++
@@ -1,9 +0,0 @@
-import {prettyJSON} from "../ms-ti-base/output";
-
-const scraper = require('table-scraper');
-scraper
-    .get('https://highshortinterest.com/')
-    .then(function(tableData:string) {
-        //console.log(tableData)
-        console.log(prettyJSON(tableData))
-    });
