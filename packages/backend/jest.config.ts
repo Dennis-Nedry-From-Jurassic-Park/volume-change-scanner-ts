b6e0d6a18b39b25dd020403c57b0613c41a6437d
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2022-09-11 19:22:24 +0300

    up jest config from old config.json

--- packages/backend/jest.config.ts
+++ packages/backend/jest.config.ts
@@ -4,18 +4,22 @@ import type {Config} from '@jest/types';
 const config: Config.InitialOptions = {
     verbose: true,
     bail: 15,
+    preset: "ts-jest",
+    testEnvironment: "node",
     transform: {
-        ".(ts|tsx)": "ts-jest"
+        ".(ts|tsx)": "ts-jest",
+        "^.+\\.(t|j)s$": "ts-jest"
     },
     globals: {
         "testPathIgnorePatterns ": [
+            "/.cache.ti.api.gs.ru.shares.report/",
             "/assets/",
+            "/benchmark/",
             "/dist/",
             "/node_modules/"
         ],
         "testMatch": [
-            "*.test.(ts|js)",
-            "*.spec.(ts|js)"
+            "*.(test|spec).(ts|js)"
         ],
         "ts-jest": {
             "tsconfig": "tsconfig.json",
