22f3d5b7fdbc0ba18ae356b74c7167abeedb9ab5
Dennis-Nedry-From-Jurassic-Park <dennis.nedry.from.jurassic.park@gmail.com>
2022-09-11 16:02:50 +0300

    auth module

--- packages/backend/apps/ms-cron/src/portfolio.controller.ts
+++ packages/backend/apps/ms-cron/src/portfolio.controller.ts
@@ -1,11 +1,22 @@
-import {Controller, Get} from '@nestjs/common';
+import {Controller, Get, Header} from '@nestjs/common';
+import {prettyJSON} from "../../ms-ti-base/output";
+import {health} from "./tasks/tasks.constants";
+import {ApiBearerAuth} from "@nestjs/swagger";
 //import {exec_portfolio_balance_update} from "./portfolio.service";
 
 @Controller('cron')
+@ApiBearerAuth('token')
 export class PortfolioController {
     @Get('/exec-portfolio-balance-update')
     async exec_portfolio_balance_update() {
         //return await exec_portfolio_balance_update();
+    }
+    @Get('/health')
+    @Header('content-type', 'application/json')
+    async get_health() {
+        const obj = await health.get_health();
+        return obj// JSON.stringify(obj)
+        //JSON.parse(prettyJSON(health.get_health()));
     }
 
 
